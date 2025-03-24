eb779001336cd1a6c0fa8ff4798721a232f93b20
Richard <olella93@gmail.com>
2025-03-24 12:42:58 +0300

    updated

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Retrieving the new id value
+        const newId = data.id;
+        
+        // Appending the new id value to the DOM
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = newId;
+        body.appendChild(p);
+    })
+    .catch(error => {
+        // Appending the error message to the DOM
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = "Unauthorized Access";
+        body.appendChild(p);
+    });
+}
